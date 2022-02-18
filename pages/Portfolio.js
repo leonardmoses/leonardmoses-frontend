ddf0a5ee04ff039181e073b52b637f50285e0e9f
Leonard Moses <qalexeon@Xavier.lan>
2022-02-18 18:21:17 -0500

    created links from home to portfolio and back.

---
+++ pages/Portfolio.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+function Portfolio() {
+    return ( 
+        <div>
+
+            <h1>Portfolio</h1>
+
+            <Link href="/">Home</Link>
+
+        </div>
+     );
+}
+
+export default Portfolio;
