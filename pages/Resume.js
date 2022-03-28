2792cc14453861a7496c06195fbb97b91f119570
Leonard Moses <qalexeon@Xavier.lan>
2022-03-28 18:28:51 -0400

    basic styling for Resume done. Button created with link to download resume.

--- pages/Resume.js
+++ pages/Resume.js
@@ -1,18 +1,28 @@
 import Link from "next/link"
 import { motion } from 'framer-motion'
 import Layout from "../components/Layout";
+import styles from "./Resume.module.scss";
 
 
 function Resume() {
     return ( 
         <Layout>
-            <div>
-                <a href="../static/images/Moses_Resume.pdf" download>
-                    <button>Download</button>
-                </a>
-                <img src="../static/images/Moses_Resume.png"/>
-            </div>
+            <main className={styles.main}>
+                <div className={styles.content}>
+                    <div className={styles.subheader}>
+                        <h1>Resume</h1>
+                        <div className={styles.downloadDiv}>
+                            <h2>Updated April 2022</h2>
+                            <a href="../static/images/Moses_Resume.pdf" download className={styles.downloadButton}>Download Resume</a>
+                        </div>
 
+                    </div>
+                    <div className={styles.resume}>
+                        <img src="../static/images/Moses_Resume.png"/>
+                    </div>
+                    
+                </div>
+            </main>
         </Layout>
      );
 }
